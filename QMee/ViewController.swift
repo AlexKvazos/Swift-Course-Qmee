5f04bf3162ccdd53d25fedfa0464ffbfbaeedd33
AlexKvazos <contact@alexkvazos.com>
2017-11-01 00:28:17 -0600

    Initial Commit

---
+++ QMee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QMee
+//
+//  Created by Alejandro Cavazos on 11/1/17.
+//  Copyright © 2017 Alejandro Cavazos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
